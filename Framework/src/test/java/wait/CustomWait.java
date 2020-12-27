9fd402a8eec03f8ba6f544bbbf8391f9d1c392f4
Anton Homtsov <dandyforwork@gmail.com>
2020-12-27 17:44:34 +0300

    fixed timeouts for CI

--- Framework/src/test/java/wait/CustomWait.java
+++ Framework/src/test/java/wait/CustomWait.java
@@ -10,7 +10,7 @@ public class CustomWait {
     private static final int WAIT_TIMEOUT_SECONDS = 20;
 
     public static void waitForPageLoaded(WebDriver driver){
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
                 .until(CustomCondition.jQueryAJAXsCompleted());
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
